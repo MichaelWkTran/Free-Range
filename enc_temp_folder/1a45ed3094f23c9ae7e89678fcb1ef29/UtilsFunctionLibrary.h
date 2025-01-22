d43d9499f3dddfa2f94a7aa061ef9a74e4d17d34
MichaelWkTran <88115414+MichaelWkTran@users.noreply.github.com>
2025-01-23 12:24:59 +1300

    Refined Car Obstacle AI 2025.01.23
    Cars will slow down when approaching a curve allowing for more accurate AI

---
+++ enc_temp_folder/1a45ed3094f23c9ae7e89678fcb1ef29/UtilsFunctionLibrary.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "UtilsFunctionLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHIKENRAGEGAME_API UUtilsFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
