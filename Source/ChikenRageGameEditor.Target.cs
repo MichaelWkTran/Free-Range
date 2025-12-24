2bf8aff84ce05a77475e21412a804848bed72cd2
MichaelWkTran <88115414+MichaelWkTran@users.noreply.github.com>
2025-12-24 18:31:36 +1300

    To UE 5.6

--- Source/ChikenRageGameEditor.Target.cs
+++ Source/ChikenRageGameEditor.Target.cs
@@ -8,8 +8,8 @@ public class ChikenRageGameEditorTarget : TargetRules
 	public ChikenRageGameEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
 		ExtraModuleNames.Add("ChikenRageGame");
 	}
 }
